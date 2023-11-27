d1e2e716e8687312be29252e2195ca71bdd36f44
Santiago <santiago@plqi33.soc.lip6.fr>
2023-11-27 16:47:19 +0100

    It checks for every folder inside the directory, recursively.

--- aux.py
+++ aux.py
@@ -5,11 +5,20 @@ Created on Wed Jun 21 15:24:23 2023
 
 @author: santiago
 """
-
 import os
+import itertools
 
 def check_dir(path):
-    is_dir = os.path.isdir(path)
-    if(not is_dir):
-        os.mkdir(path)
-        print('Created:', path)
+    directories = path.split(os.sep)
+    [directories.insert(2*i+1, os.sep) for i in range(len(directories)-1)]
+    directories = list(itertools.accumulate(directories))
+    
+    for d in directories:
+        is_dir = os.path.isdir(d)
+        if(not is_dir):
+            os.mkdir(d)
+            print('Created:', d)
+
+if __name__ == '__main__':
+    path = 'a/b/c/d/e'
+    check_dir(path)
